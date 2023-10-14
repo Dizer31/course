caf1af34a1add1053a0604915efe5098f271112c
Dizer31 <82336861+Dizer31@users.noreply.github.com>
2023-10-15 02:41:20 +0300

    Add files via upload

---
+++ Lab1/2.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+len_ = len(list_players)
+list1 = list_players[:(len_ // 2)]
+list2 = list_players[(len_ // 2):]
+print(list1)
+print(list2)
+# TODO Разделите участников на две команды
