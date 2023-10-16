3d85b28516c7554e18c9b8a5354e31076bc0c04a
Dizer31 <82336861+Dizer31@users.noreply.github.com>
2023-10-17 02:22:00 +0300

    Update 2.py

--- Lab1/2.py
+++ Lab1/2.py
@@ -1,7 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 len_ = len(list_players)
-list1 = list_players[:(len_ // 2)]
-list2 = list_players[(len_ // 2):]
+middle = len_ // 2
+list1 = list_players[:middle]
+list2 = list_players[middle:]
 print(list1)
 print(list2)
 # TODO Разделите участников на две команды
